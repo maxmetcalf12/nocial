4f1decbde2ef5fa13a77cb96fb9c598c08f6c96f
Max Metcalf <maxmetcalf12@gmail.com>
2020-12-10 13:28:32 -0800

    Initial landing page.

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   darkMode: 'media',
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"IBM Plex Sans"', ...defaultTheme.fontFamily.sans],
+        'mono': ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono]
+      },
+    },
   },
   variants: {},
   plugins: [],
