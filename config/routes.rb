f0d7a68d97b2cd0b161d4784d5c44eced980e9b4
Max Metcalf <maxmetcalf12@gmail.com>
2021-01-04 22:25:29 -0700

    Added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  devise_for :users
   root "home#index"
   get "pricing", to: "home#pricing"
   get "manifesto", to: "home#manifesto"
